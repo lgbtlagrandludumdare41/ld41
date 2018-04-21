f8e8454f01ca462de77f78e938766aeecae2d2ea
Aaron Boudreaux <aaronmboudreaux@gmail.com>
2018-04-21 15:43:24 -0700

    Adding uncovering logic.

--- java/ld41/Minesweeper.java
+++ java/ld41/Minesweeper.java
@@ -13,12 +13,22 @@ import java.util.Random;
 public class Minesweeper 
 {
     public static final int DEFAULT_WIDTH_HEIGHT = 10;
-	public static final char SPACE_EMPTY = '_';
-	public static final char SPACE_FLAG = 'F';
+	public static final char SPACE_EMPTY = 'x';
+	public static final char SPACE_FLAG = 'f';
 	public static final char SPACE_UNKNOWN = '#';
-	public static final char SPACE_BOMB = 'B';
+	public static final char SPACE_BOMB = 'b';
 	public static final char SPACE_INVALID = '*';
 	public static final char SPACE_QUESTION = '?';
+	
+	public static final String CMD_CLICK = "c";
+	public static final String CMD_DEBUG = "d";
+	public static final String CMD_FLAG = "f";
+	public static final String CMD_QUESTION = "?";
+	public static final String CMD_REMOVE = "x";
+	
+	public static final String RESULT_INVALID = "invalid.\n";
+	public static final String RESULT_WIN = "win.\n";
+	public static final String RESULT_LOSE = "bomb.\n";
     
     private char[][] _fullBoard;
     private char[][] _userBoard;
@@ -67,10 +77,42 @@ public class Minesweeper
 				_fullBoard[row][col] = SPACE_BOMB;
 				numBombs++;
 			}
-			// Otherwise, don't do anything - just roll another (x, y) next time
+			// Otherwise, don't do anything - just try again next iteration
 		}
 		
-		prettyPrintFullGrid(); // For testing only
+		// TODO: Update numbers 
+		int adjacentBombs;
+		
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				adjacentBombs = 0;
+				for (int i = (x - 1); i < (x + 2); i++)
+				{
+					for (int j = (y - 1); j < (y + 2); j++)
+					{
+						if (i >= 0 && i < _fullBoard.length &&
+							j >= 0 && j < _fullBoard[0].length)
+						{
+							if (i != x || j != y)
+							{
+								if (_fullBoard[i][j] == SPACE_BOMB)
+								{
+									adjacentBombs++;
+								}
+							}
+						}
+					}
+				}
+				if (_fullBoard[x][y] != SPACE_BOMB && adjacentBombs > 0)
+				{
+					_fullBoard[x][y] = Integer.toString(adjacentBombs).charAt(0);
+				}
+			}
+		}
+		
+//		prettyPrintFullGrid(); // For testing only
     }
 
 	/**
@@ -81,9 +123,9 @@ public class Minesweeper
 	 */
     public String processCommand(String command, String values)
 	{
-		if (command.toLowerCase().equals("d"))
+		if (command.toLowerCase().equals(CMD_DEBUG))
 		{
-			return getUserGrid() + "\n" + getFullGrid();
+			return "debug(\'" + getUserGrid() + "\', \'" + getFullGrid() + "\').\n";
 		}
 		else if (command.toLowerCase().equals("u"))
 		{
@@ -97,7 +139,8 @@ public class Minesweeper
 		}
 		else if (!values.contains(",") || values.length() < 3)
 		{
-			return "Invalid";
+//			System.err.println("values invalid");
+			return RESULT_INVALID;
 		}
 		else
 		{
@@ -108,27 +151,38 @@ public class Minesweeper
 			String result;
 			if (x >= 0 && x < _fullBoard.length && y >= 0 && y < _fullBoard[0].length)
 			{
-				if (command.toLowerCase().equals("c"))
+//				System.err.println("Command: " + command + ".");
+				if (command.toLowerCase().equals(CMD_CLICK))
 				{
 					result = clickSquare(x, y);
 				}
-				else if (command.toLowerCase().equals("f"))
+				else if (command.toLowerCase().equals(CMD_FLAG))
 				{
 					result = flagSquare(x, y);
 				}
-				else if (command.equals("?"))
+				else if (command.equals(CMD_QUESTION))
 				{
 					_userBoard[x][y] = SPACE_QUESTION;
 					result = peek(x, y);
 				}
+				else if (command.toLowerCase().equals(CMD_REMOVE))
+				{
+					if (_userBoard[x][y] == SPACE_FLAG || _userBoard[x][y] == SPACE_QUESTION)
+					{
+						_userBoard[x][y] = SPACE_UNKNOWN;
+					}
+					result = peek(x, y);
+				}
 				else
 				{
-					result = "Invalid";
+//					System.err.println("invalid command");
+					result = RESULT_INVALID;
 				}
 			}
 			else
 			{
-				result = "Invalid";
+//				System.err.println("Invalid coordinates");
+				result = RESULT_INVALID;
 			}
 
 			return result;
@@ -147,15 +201,18 @@ public class Minesweeper
 		
 		if (_userBoard[x][y] == SPACE_BOMB)
 		{
-			return "Bomb";
+			return RESULT_LOSE;
 		}
 		if (allBombsFlagged())
 		{
-			return "Win";
+			return RESULT_WIN;
 		}
 		
 		// Update the user board to uncover any blank spaces
-		uncoverBlanks();
+		if (_userBoard[x][y] == SPACE_EMPTY)
+		{
+			uncoverBlanks(x, y);
+		}
 		return peek(x, y);
 	}
 	
@@ -171,7 +228,7 @@ public class Minesweeper
 		
 		if (allBombsFlagged())
 		{
-			return "Win";
+			return RESULT_WIN;
 		}
 		
 		return peek(x, y);
@@ -187,9 +244,11 @@ public class Minesweeper
 		{
 			for (int j = 0; j < _fullBoard[0].length; j++)
 			{
-				if (_fullBoard[i][j] == SPACE_BOMB && _userBoard[i][j] != SPACE_FLAG)
+				// If a bomb is not flagged or a flag is NOT on a bomb, then just bail out now
+				if ((_fullBoard[i][j] == SPACE_BOMB && _userBoard[i][j] != SPACE_FLAG) ||
+					(_userBoard[i][j] == SPACE_FLAG && _fullBoard[i][j] != SPACE_BOMB))
 				{
-					System.out.println("Unflagged bomb at " + i + ", " + j);
+//					System.err.println("Unflagged bomb at " + i + ", " + j);
 					return false;
 				}
 			}
@@ -199,12 +258,80 @@ public class Minesweeper
 		return true;
 	}
 	
-	/**
+	/** Called after the user clicked a square to uncover any connected blanks
 	 * 
 	 */
-	private void uncoverBlanks()
+	private void uncoverBlanks(int x, int y)
 	{
+//		System.err.println("Checking around " + x + " " + y);
+		// Count bombs adjacent to this square
+		// If count is 0, uncover all adjacent squares
+		// and make a recursive call to this function for each that's not already uncovered
+		boolean adjacentBombs = false;
+		int[][] locationsToCheck = new int[8][2];
+		int next = 0;
 		
+		for (int i = (x - 1); i < (x + 2); i++)
+		{
+			for (int j = (y - 1); j < (y + 2); j++)
+			{
+				if (i >= 0 && i < _userBoard.length &&
+					j >= 0 && j < _userBoard[0].length)
+				{
+//					System.err.println(i + " " + j + " " + _fullBoard[i][j] + " " + _userBoard[i][j]);
+					if (i != x || j != y)
+					{
+						if (_fullBoard[i][j] == SPACE_BOMB)
+						{
+							adjacentBombs = true;
+							break;
+						}
+						else
+						{
+							locationsToCheck[next][0] = i;
+							locationsToCheck[next][1] = j;
+//							System.err.println("Check " + i + " " + j);
+							next++;
+						}
+					}
+				}
+			}
+		}
+		
+		if (!adjacentBombs)
+		{
+			boolean[] processLocation = new boolean[8];
+			for (int i = 0; i < next; i++)
+			{
+				int xCoord = locationsToCheck[i][0];
+				int yCoord = locationsToCheck[i][1];
+				
+				processLocation[i] = false;
+				if (_userBoard[xCoord][yCoord] == SPACE_UNKNOWN)
+				{
+//					System.err.println("Uncover " + xCoord + " " + yCoord);
+					_userBoard[xCoord][yCoord] = _fullBoard[xCoord][yCoord];
+					processLocation[i] = true;
+				}
+			}
+			
+			for (int k = 0; k < next; k++)
+			{
+//				System.err.print("Does " + locationsToCheck[k][0] + " " + locationsToCheck[k][1] + " need more checking?");
+				if (processLocation[k])
+				{
+//					System.err.println("Yes");
+					int xCoord = locationsToCheck[k][0];
+					int yCoord = locationsToCheck[k][1];
+
+					uncoverBlanks(xCoord, yCoord);
+				}
+//				else
+//				{
+//					System.err.println("No");
+//				}
+			}
+		}
 	}
 
 	/**
@@ -278,6 +405,7 @@ public class Minesweeper
 	private void prettyPrintUserGrid()
 	{
 		String s = "";
+		
 		for (char[] row : _userBoard)
 		{
 			for (char square : row)
