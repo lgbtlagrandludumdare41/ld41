82cfbb36e65a207aff14222ab243a3ea1aa09ee8
Aaron Boudreaux <aaronmboudreaux@gmail.com>
2018-04-21 10:47:46 -0700

    Fixed bug in shell script

--- java/run.sh
+++ java/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 javac Ld41.java
-width = 10
-height = 10
-if [$1 != ""]; then
-	width = $1
+width=10
+height=10
+if [ "$1" != "" ]; then
+	width=$1
 fi
 
-if [$2 != ""]; then
-	height = $2
+if [ "$2" != "" ]; then
+	height=$2
 fi
 
-java Ld41 $1 $2
+java Ld41 $width $height
