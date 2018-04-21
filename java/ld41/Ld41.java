35bbe3ec818535ac4a63dd197783ae3d6b0ec7fd
Aaron Boudreaux <aaronmboudreaux@gmail.com>
2018-04-21 12:36:01 -0700

    New boards are populated with 10% bombs.

--- java/ld41/Ld41.java
+++ java/ld41/Ld41.java
@@ -18,29 +18,37 @@ public class Ld41
      */
     public static void main(String[] args)
     {
-		int width = 10;
-		int height = 10;
+		int width = Minesweeper.DEFAULT_WIDTH_HEIGHT;
+		int height = Minesweeper.DEFAULT_WIDTH_HEIGHT;
 
-		if (args.length >= 2)
+		if (args.length >= 1)
 		{
 			width = Integer.parseInt(args[0]);
 		}
-		else if (args.length >= 3)
+		if (args.length >= 2)
 		{
 			height = Integer.parseInt(args[1]);
 		}
+		
+		System.out.println("Width: " + width);
+		System.out.println("Height: " + height);
 
 		Minesweeper game = new Minesweeper(width, height);
 
 		Scanner sc = new Scanner(System.in);
-		String s, command, values;
+		String s;
 		s = sc.nextLine();
 		while (!s.equals("exit") && !s.equals("quit")) // Just for testing purposes
 			{
-				String[] params = s.split(" ");
-				command = params[0];
-				values = params[1];
-				System.out.println(game.processCommand(command, values));
+				try
+				{
+					String[] params = s.split(" ");
+					System.out.println(game.processCommand(params[0], params[1]));
+				}
+				catch (Exception e)
+				{
+					System.out.println("Invalid");
+				}
 				s = sc.nextLine();
 			}
 		}
--- java/ld41/Minesweeper.java
+++ java/ld41/Minesweeper.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ld41;
+import java.util.Random;
 
 /**
  *
@@ -19,8 +20,6 @@ public class Minesweeper
 	public static final char SPACE_INVALID = '*';
 	public static final char SPACE_QUESTION = '?';
     
-    private int _width;
-    private int _height;
     private char[][] _fullBoard;
     private char[][] _userBoard;
 	
@@ -30,21 +29,39 @@ public class Minesweeper
     }
     
     public Minesweeper(int width, int height)
-    {
-		_width = width;
-		_height = height;
+    {	
+		_fullBoard = new char[width][height];
+		_userBoard = new char[width][height];
 		
-		_fullBoard = new char[_width][_height];
-		_userBoard = new char[_width][_height];
-		
-		for (int i = 0; i < _width; i++)
+		// Initialize the boards. The user can't see anything and the real board is empty
+		for (int i = 0; i < width; i++)
 		{
-			for (int j = 0; j < _height; j++)
+			for (int j = 0; j < height; j++)
 			{
 				_fullBoard[i][j] = SPACE_EMPTY;
 				_userBoard[i][j] = SPACE_UNKNOWN;
 			}
 		}
+		
+		// Add some bombs! 10% of the squares will contain a bomb
+		int numBombs = 0;
+		int maxBombs = (int) Math.round(width * height * 0.1);
+		Random r = new Random(System.currentTimeMillis());
+		while (numBombs < maxBombs)
+		{
+			int row = r.nextInt(width);
+			int col = r.nextInt(height);
+			
+			// If there isn't already a bomb here, put one
+			if (_fullBoard[row][col] == SPACE_EMPTY)
+			{
+				_fullBoard[row][col] = SPACE_BOMB;
+				numBombs++;
+			}
+			// Otherwise, don't do anything - just roll another (x, y) next time
+		}
+		
+		prettyPrintFullGrid();
     }
 
     public String processCommand(String command, String values)
@@ -53,6 +70,16 @@ public class Minesweeper
 		{
 			return getUserGrid() + "\n" + getFullGrid();
 		}
+		else if (command.toLowerCase().equals("u"))
+		{
+			prettyPrintUserGrid();
+			return "";
+		}
+		else if (command.toLowerCase().equals("a"))
+		{
+			prettyPrintFullGrid();
+			return "";
+		}
 		else if (!values.contains(",") || values.length() < 3)
 		{
 			return "Invalid";
@@ -60,15 +87,16 @@ public class Minesweeper
 		else
 		{
 			String[] coordinates = values.split(",");
-			int x = Integer.parseInt(coordinates[0]);
-			int y = Integer.parseInt(coordinates[1]);
+			// Convert into 0-indexed
+			int x = Integer.parseInt(coordinates[0]) - 1;
+			int y = Integer.parseInt(coordinates[1]) - 1;
 
 			String result;
-			if (x > 0 && x < _width && y > 0 && y < _height)
+			if (x >= 0 && x < _fullBoard.length && y >= 0 && y < _fullBoard[0].length)
 			{
 				if (command.toLowerCase().equals("c"))
 				{
-					result = peek(x, y);
+					result = clickBoard(x, y);
 				}
 				else if (command.toLowerCase().equals("f"))
 				{
@@ -93,40 +121,94 @@ public class Minesweeper
 			return result;
 		}
 	}
+	
+	public String clickBoard(int x, int y)
+	{
+		String s = "";
+		
+		return s;
+	}
 
 	public String getUserGrid()
 	{
-		String grid = "";
+		String s = "";
 		
-		for (int i = 0; i < _width; i++)
+		for (int i = 0; i < _userBoard.length; i++)
 		{
-			for (int j = 0; j < _height; j++)
+			for (int j = 0; j < _userBoard[0].length; j++)
 			{
-				grid += _userBoard[i][j];
+				s += _userBoard[i][j];
 			}
 		}
 
-		return grid;
+		return s;
 	}
 	
 	public String getFullGrid()
 	{
-		String grid = "";
+		String s = "";
 
-		for (int i = 0; i < _width; i++)
+		for (int i = 0; i < _fullBoard.length; i++)
 		{
-			for (int j = 0; j < _height; j++)
+			for (int j = 0; j < _fullBoard[0].length; j++)
 			{
-				grid += _fullBoard[i][j];
+				s += _fullBoard[i][j];
 			}
 		}
 
-		return grid;
+		return s;
 	}
 	
 	public String peek(int x, int y)
 	{
-		// Just return test data for now
-		return "_FB#?123";
+		String s = "";
+		
+		for (int i = (x - 1); i < (x + 2); i++)
+		{
+			for (int j = (y - 1); j < (y + 2); j++)
+			{
+				if (i < 0 || j < 0 || i >= _userBoard.length || j >= _userBoard[0].length)
+				{
+					s += SPACE_INVALID;
+				}
+				else
+				{
+					s += _userBoard[i][j];
+				}
+			}
+		}
+		
+		return s;
+	}
+	
+	private void prettyPrintUserGrid()
+	{
+		String s = "";
+		for (int i = 0; i < _userBoard.length; i++)
+		{
+			for (int j = 0; j < _userBoard[0].length; j++)
+			{
+				s += _userBoard[i][j] + " ";
+			}
+			s += "\n";
+		}
+		
+		System.out.print(s);
+	}
+	
+	private void prettyPrintFullGrid()
+	{
+		String s = "";
+		for (char[] _fullBoardRow : _fullBoard)
+		{
+			for (char _square : _fullBoardRow)
+			//for (int j = 0; j < _fullBoard[0].length; j++)
+			{
+				s += _square + " ";
+			}
+			s += "\n";
+		}
+		
+		System.out.print(s);
 	}
 }
